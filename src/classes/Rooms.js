0335d4abc12565b2dcbb3a53f74720360add718e
Oscar Cortez <oacortez98@gmail.com>
2022-01-14 22:26:37 -0700

    Complete class for room

--- src/classes/Rooms.js
+++ src/classes/Rooms.js
@@ -1,9 +1,13 @@
-class Rooms {
-  constructor() {
-    
+class Room {
+  constructor(roomsData) {
+    this.number = roomsData.number;
+    this.roomType = roomsData.roomType;
+    this.bidet = roomsData.bidet || false;
+    this.bedSize = roomsData.bedSize;
+    this.numOfBeds = roomsData.numBeds;
+    this.costPerNight = roomsData.costPerNight;
   }
 }
 
 
-
-export default Rooms;
+export default Room;
