0f2158d490490c43b0d3e46591133c0184b47f88
Oscar Cortez <oacortez98@gmail.com>
2022-01-18 13:33:33 -0700

    Add function for customer load

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,44 @@
 import {customersData, roomsData, bookingsData, userData, postData} from './api-calls';
 import './css/base.scss';
-import Hotel from '../classes/Hotel';
+import Hotel from './classes/Hotel';
+import domUpdates from './domUpdates';
+
+// Query Selectors:
+const title = document.getElementById('title');
+const roomTypeTags = document.getElementById('filterRoom');
+const calendar = document.getElementById('dateCalander');
+
+
+// Buttons || icons:
+const homeBtn = document.querySelector('.home-btn');
+const myProfileBtn = document.getElementById('myProfileBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+const submitRequestBtn = document.getElementById('submitRequestBtn');
 
 let hotel; 
 
 const getAllData = (userID) => {
   Promise.all([customersData(), roomsData(), bookingsData(), userData(userID)])
   .then(data => {
-    hotel = new Hotel(data[0], data[2], data[1])
+    hotel = new Hotel(data[0].customers, data[2].bookings, data[1].rooms)
+    hotel.getAllUserBookings(data[3])
+    hotel.getTotalPrice()
+    loadCustomerInfo()
   })
 }
+
+// Event Listeners:
+window.addEventListener('load', () => {
+  getAllData(1)
+});
+
+
+// Functions: 
+
+const loadCustomerInfo = () => {
+  domUpdates.displayWelcomeMessage(hotel.currentCustomer.name, hotel.currentCustomer.totalSpent);
+}
+
+
+
+export default hotel;
