50c53871fb6ae9a92fe28a5e752f0c380fa875a6
Oscar Cortez <oacortez98@gmail.com>
2022-01-18 22:55:12 -0700

    Fix bug to display card when filtering

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -5,15 +5,15 @@ const hide = elements => elements.forEach(element => element.classList.add('hidd
 
 
 const domUpdates = {
-  displayWelcomeMessage(customer, totalSpent) {
-    const welcomeMessage = document.getElementById('welcomeMessage');
+  displayWelcomeMessage(customer, totalSpent, homeView) {
+    const welcomeMessage = document.querySelector('#welcomeMessage');
     welcomeMessage.innerText = `Welcome ${customer}, Your total amount spent on rooms: $${totalSpent}`;
+    show([homeView])
   },
 
-  displayAllUsersBookings(currentCustomerBookings, image) {
-    const profileView = document.getElementById('profileView');
+  displayAllUsersBookings(currentCustomerBookings, image, homeView) {
+    const profileView = document.querySelector('#profileView');
     profileView.innerHTML = '';
-    console.log(currentCustomerBookings);
     currentCustomerBookings.forEach(booking => {
       profileView.innerHTML += `
       <article class="customerCard">
@@ -24,8 +24,43 @@ const domUpdates = {
       </article>
       `
     })
+    show([profileView]);
+    hide([homeView])
   },
 
+  displayAllAvailableRooms(availableRooms, availableRoomsView, homeView) {
+    if(availableRooms.length > 0) {
+    const availableRoomsView = document.querySelector('#availableRoomsView');
+    availableRoomsView.innerHTML = '';
+    availableRooms.forEach(room => {
+    availableRoomsView.innerHTML += `
+    <article class="available-room-card">
+      <h3>Room Details</h3>
+      <p>Room Type: ${room.roomType}</p>
+      <p>Bed Size: ${room.bedSize}</p>
+      <p>Bidet: ${room.bidet}</p>
+      <p>Room Number: ${room.number}</p>
+      <p>Cost Per Night: ${room.costPerNight}</p>
+
+      // Create a button to add functionality to add this room card to users
+      //  array. ⬇️⬇️
+      <button value=${room.number} class= ><i class="fa fa-plus-circle add-card-btn"></i></button>
+    </article>
+    `
+    })
+    }
+    if(availableRooms.length < 1 ) {
+      const availableRoomsView = document.querySelector('#availableRoomsView');
+      availableRoomsView.innerHTML = '';
+      availableRoomsView.innerHTML += `<p class="card-err-msg">We are so sorry, but we do not have any availablity rooms for that room type on that date. Please make another selection!</p>`
+    }
+    show([availableRoomsView]);
+    hide([homeView])
+  },
+
+  addRoomCard() {
+
+  }
 }
 
 export default domUpdates;
