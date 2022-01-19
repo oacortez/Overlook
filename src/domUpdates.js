15401cb7a3197e37e23d0ee633ebb0d73f35c7b1
Oscar Cortez <oacortez98@gmail.com>
2022-01-19 11:47:02 -0700

    Add displayAllAvailableRooms function

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,4 +1,11 @@
 import hotel from './scripts';
+import {bookRoom} from './scripts';
+
+// let cardBtns; 
+const availableRoomsView = document.querySelector('#availableRoomsView');
+const profileView = document.querySelector('#profileView');
+const welcomeMessage = document.querySelector('#welcomeMessage');
+
 
 const show = elements => elements.forEach(element => element.classList.remove('hidden'));
 const hide = elements => elements.forEach(element => element.classList.add('hidden'));
@@ -6,13 +13,11 @@ const hide = elements => elements.forEach(element => element.classList.add('hidd
 
 const domUpdates = {
   displayWelcomeMessage(customer, totalSpent, homeView) {
-    const welcomeMessage = document.querySelector('#welcomeMessage');
     welcomeMessage.innerText = `Welcome ${customer}, Your total amount spent on rooms: $${totalSpent}`;
     show([homeView])
   },
 
-  displayAllUsersBookings(currentCustomerBookings, image, homeView) {
-    const profileView = document.querySelector('#profileView');
+  displayAllUsersBookings(currentCustomerBookings, image, homeView, profileView, availableRoomsView) {
     profileView.innerHTML = '';
     currentCustomerBookings.forEach(booking => {
       profileView.innerHTML += `
@@ -25,12 +30,11 @@ const domUpdates = {
       `
     })
     show([profileView]);
-    hide([homeView])
+    hide([homeView, availableRoomsView]);
   },
 
   displayAllAvailableRooms(availableRooms, availableRoomsView, homeView) {
     if(availableRooms.length > 0) {
-    const availableRoomsView = document.querySelector('#availableRoomsView');
     availableRoomsView.innerHTML = '';
     availableRooms.forEach(room => {
     availableRoomsView.innerHTML += `
@@ -41,10 +45,7 @@ const domUpdates = {
       <p>Bidet: ${room.bidet}</p>
       <p>Room Number: ${room.number}</p>
       <p>Cost Per Night: ${room.costPerNight}</p>
-
-      // Create a button to add functionality to add this room card to users
-      //  array. ⬇️⬇️
-      <button value=${room.number} class= ><i class="fa fa-plus-circle add-card-btn"></i></button>
+      <button value=${room.number} class="cardBtns">Submit</button>
     </article>
     `
     })
@@ -55,12 +56,8 @@ const domUpdates = {
       availableRoomsView.innerHTML += `<p class="card-err-msg">We are so sorry, but we do not have any availablity rooms for that room type on that date. Please make another selection!</p>`
     }
     show([availableRoomsView]);
-    hide([homeView])
+    hide([homeView, ])
   },
-
-  addRoomCard() {
-
-  }
 }
 
 export default domUpdates;
